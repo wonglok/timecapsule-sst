50ff97ad20f4a43ae73f5d15c0f29854c41dc951
Lok Lok <yellowhappy831@gmail.com>
2025-08-01 16:19:33 +0800

    save

---
+++ src/rest/auth/check-jwt-ok.ts
@@ -0,0 +1,43 @@
+import { jwt2data } from "./util/jwt2data2jwt";
+// import { server } from "@passwordless-id/webauthn";
+// import { base64url } from "jose";
+import { Resource } from "sst";
+// import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+// import { unmarshall } from "@aws-sdk/util-dynamodb";
+
+// let dyna = new DynamoDBClient();
+
+export async function handler(event: any) {
+    let { jwt }: any = JSON.parse(event.body);
+
+    if (!jwt) {
+        return {
+            ok: false,
+        };
+    }
+
+    let resp = await jwt2data({
+        payload: jwt,
+        secretKey: Resource.SESSION_SECRET.value,
+    })
+        .then((r) => {
+            return {
+                ok: true,
+            };
+        })
+        .catch((e) => {
+            return {
+                ok: false,
+            };
+        });
+
+    if (!resp.ok) {
+        return {
+            ok: false,
+        };
+    } else {
+        return {
+            ok: true,
+        };
+    }
+}
