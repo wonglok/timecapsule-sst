cc746c6707f1f78572db27c4c670c1d5536cb98e
Lok Lok <yellowhappy831@gmail.com>
2025-08-02 15:27:41 +0800

    save

--- src/rest/auth/request-email.ts
+++ src/rest/auth/request-email.ts
@@ -10,10 +10,18 @@ import { Resource } from "sst";
 import { sendPinEmail } from "./send-pin-email";
 import { v4 } from "uuid";
 import randomize from "randomatic";
-
+import limiterFnc from "lambda-rate-limiter";
 let dyna = new DynamoDBClient();
 
+const limiter = limiterFnc({
+    interval: 60 * 1000, // Rate limit interval in ms (e.g., 1 minute)
+    uniqueTokenPerInterval: 500, // Max requests per interval
+});
+
 export async function handler(event) {
+    const userToken = event?.requestContext?.identity?.sourceIp || "anonymous"; // Or another identifier
+    await limiter.check(50, userToken);
+
     let inbound = JSON.parse(event.body);
 
     let user = await dyna
